2c05c81c07f7e0d547f987849b4d6a8d91434fe1
starsdong <XDong@lbl.gov>
2023-02-21 11:50:30 -0800

    - fixed a bug in fixing parameters in Minuit fitting - added an option for drawing with some pre-set parameters - a few more labels added for different purposes

--- Hypernuclei/dLambda/Ckstar.C
+++ Hypernuclei/dLambda/Ckstar.C
@@ -1,11 +1,14 @@
 #include "style.C+"
 #include "draw.C+"
 //#define __EFF__  // allow efficiency for doublet?
+#define __FIT__
+//#define __CONTOUR__
 
 const double hbarc = 197.327;  // MeV * fm
 const double sqrtPi = TMath::Sqrt( TMath::Pi() );
 const Int_t NC = 3;
-const Int_t NP = 20;  // read in 40 points
+const Int_t NP = 40;  // read in 60 points
+const Double_t KMAX = NP*5.0;  // K* max
 #ifdef __EFF__
 const Int_t NPar = 8;  // R0, R1, R2, f00, d00, f01, d01, eff
 #else
@@ -97,8 +100,10 @@ void draw()
 {
   style();
 
-  const Double_t par0[2][2] = {{-16.8, 2.3}, {-16.3, 3.2}};
-  const Double_t par1[3][2] = {{17.3, 3.6}, {10.8, 3.8}, {7.6, 3.6}};
+  //  const Double_t par0[2][2] = {{-16.8, 2.3}, {-16.3, 3.2}};
+  //  const Double_t par1[3][2] = {{17.3, 3.6}, {10.8, 3.8}, {7.6, 3.6}};
+  const Double_t par0[2][2] = {{-16.8, 2.3}, {-40, 2.3}};
+  const Double_t par1[3][2] = {{17.3, 3.6}, {17.3, 3.6}, {17.3, 3.6}};
   
   TCanvas *c1 = new TCanvas("c1","c1",0,0,800,600);
   c1->Draw();
@@ -114,9 +119,10 @@ void draw()
   const Double_t RG = 2.5;
   ofstream outData;
   for(int i=0;i<6;i++) {
+    //    if(i%3!=0) continue;
     outData.open(Form("model/dl_model_R_2.5_D_%d_Q_%d.txt", i/3, i%3));
 
-    cf[i] = new TF1(Form("func_CF_dl_%d",i),CF_dl,0,200,5);
+    cf[i] = new TF1(Form("func_CF_dl_%d",i),CF_dl,0,KMAX,5);
     cf[i]->SetParameters(RG, par0[i/3][0], par0[i/3][1], par1[i%3][0], par1[i%3][1]);
     cf[i]->SetLineWidth(2);
     cf[i]->SetLineColor(i%3+1);
@@ -124,22 +130,22 @@ void draw()
     cf[i]->SetLineStyle(i/3+1);
     cf[i]->Draw("same");
 
-    cf_0[i] = new TF1(Form("func_CF_dl_0_%d",i),CF_dl_0,0,200,3);
+    cf_0[i] = new TF1(Form("func_CF_dl_0_%d",i),CF_dl_0,0,KMAX,3);
     cf_0[i]->SetParameters(RG, par0[i/3][0], par0[i/3][1]);
     cf_0[i]->SetLineWidth(2);
     cf_0[i]->SetLineColor(i%3+1);
     //    cf_0[i]->SetLineColor(2);
     cf_0[i]->SetLineStyle(i/3+1);
-    cf_0[i]->Draw("same");
+    //    cf_0[i]->Draw("same");
 
     
-    cf_1[i] = new TF1(Form("func_CF_dl_1_%d",i),CF_dl_1,0,200,3);
+    cf_1[i] = new TF1(Form("func_CF_dl_1_%d",i),CF_dl_1,0,KMAX,3);
     cf_1[i]->SetParameters(RG, par1[i%3][0], par1[i%3][1]);
     cf_1[i]->SetLineWidth(2);
     cf_1[i]->SetLineColor(i%3+1);
     //    cf_1[i]->SetLineColor(3);
     cf_1[i]->SetLineStyle(i/3+1);
-    cf_1[i]->Draw("same");
+    //    cf_1[i]->Draw("same");
 
     for(int j=0;j<200;j++) {
       double kstar = (j+0.5);
@@ -174,7 +180,27 @@ void fcn(int &npar, double *gin, double &f, double *par,int iflag ) {
   //  cout<<" chi2/ndf="<<sum/(gr_data[0]->GetN()*NC-NPar)<<endl;
 }
 
-
+double calChi2(double *par)
+{
+  double sum=0;
+  for(int ic=0;ic<NC;ic++) {
+    //    if(ic==0) continue;
+    cf_fun[ic]->SetParameter(0, par[ic]);
+    cf_fun[ic]->SetParameter(1, par[3]);
+    cf_fun[ic]->SetParameter(2, par[4]);
+    cf_fun[ic]->SetParameter(3, par[5]);
+    cf_fun[ic]->SetParameter(4, par[6]);
+#ifdef __EFF__
+    cf_fun[ic]->SetParameter(5, par[7]);
+#endif
+    for(int i=0;i<gr_data[ic]->GetN();i++){
+      double kstar = gr_data[ic]->GetX()[i];
+      
+      sum+=pow((gr_data[ic]->GetY()[i]-cf_fun[ic]->Eval(kstar))/gr_data[ic]->GetEY()[i],2.0);
+    }
+  }
+  return sum;
+}
 
 void Ckstar()
 {
@@ -194,13 +220,13 @@ void Ckstar()
   TF1 *cf_fun_1[NC];  // quartet contribution only
   for(int ic=0;ic<NC;ic++) {
 #ifdef __EFF__
-    cf_fun[ic] = new TF1(Form("cf_fun_%d",ic),CF_dl_eff,0,200,6);
-    cf_fun_0[ic] = new TF1(Form("cf_fun_0_%d",ic), CF_dl_0_eff,0,200,4);
-    cf_fun_1[ic] = new TF1(Form("cf_fun_1_%d",ic), CF_dl_1,0,200,3);  // no difference between w/ and w/o efficiency
+    cf_fun[ic] = new TF1(Form("cf_fun_%d",ic),CF_dl_eff,0,KMAX,6);
+    cf_fun_0[ic] = new TF1(Form("cf_fun_0_%d",ic), CF_dl_0_eff,0,KMAX,4);
+    cf_fun_1[ic] = new TF1(Form("cf_fun_1_%d",ic), CF_dl_1,0,KMAX,3);  // no difference between w/ and w/o efficiency
 #else
-    cf_fun[ic] = new TF1(Form("cf_fun_%d",ic),CF_dl,0,200,5);
-    cf_fun_0[ic] = new TF1(Form("cf_fun_0_%d",ic), CF_dl_0,0,200,3);
-    cf_fun_1[ic] = new TF1(Form("cf_fun_1_%d",ic), CF_dl_1,0,200,3);
+    cf_fun[ic] = new TF1(Form("cf_fun_%d",ic),CF_dl,0,KMAX,5);
+    cf_fun_0[ic] = new TF1(Form("cf_fun_0_%d",ic), CF_dl_0,0,KMAX,3);
+    cf_fun_1[ic] = new TF1(Form("cf_fun_1_%d",ic), CF_dl_1,0,KMAX,3);
 #endif
   }
 
@@ -216,17 +242,28 @@ void Ckstar()
   double var_min[NPar] = {2.0, 2.0, 2.0, -50., 2.3, 0., 3.6, 0.0};
   double var_max[NPar] = {4.0, 4.0, 4.0, -0., 2.3, 50., 3.6, 1.0};
 #else
+#ifdef __FIT__
   //  double var[NPar] = {3.0, 2.9, 2.4, -10., 2.3, 10, 3.6};
-  double var[NPar] = {3.0, 2.9, 2.5, -20., 2.3, 10, 3.6};
+  double var[NPar] = {3.0, 2.9, 2.5, -20., 3.2, 10, 3.6};
   double verr[NPar] = {0.3, 0.3, 0.3, 0.3, 0.1, 0.3, 0.1};
-  double var_min[NPar] = {2.5, 2.5, 2.0, -50., 2.3, 3., 3.6};
-  double var_max[NPar] = {3.5, 3.5, 3.0, -3., 2.3, 50., 3.6};
+  double var_min[NPar] = {2.5, 2.5, 2.0, -50., 1.0, 0., 2.0};
+  double var_max[NPar] = {3.5, 3.5, 3.0, -0., 4.0, 20., 5.0};
+#else
+  // double var[NPar] = {2.96646, 2.8894, 2.44925, -5.68461, 2.3, 13.2108, 3.6};
+  // double verr[NPar] = {0.3, 0.3, 0.3, 0.3, 0.1, 0.3, 0.1};
+  // double var_min[NPar] = {2.96646, 2.8894, 2.44925, -5.68461, 2.3, 13.2108, 3.6};
+  // double var_max[NPar] = {2.96646, 2.8894, 2.44925, -5.68461, 2.3, 13.2108, 3.6};
+  double var[NPar] = {2.79479, 2.73791, 2.19929, -37.9044, 2.3, 6.5963, 3.6};
+  double verr[NPar] = {0.3, 0.3, 0.3, 0.3, 0.1, 0.3, 0.1};
+#endif  
 #endif
   
   double err_mat[NPar][NPar];
-  
+
+#ifdef __FIT__
   TMinuit *gMinuit = new TMinuit(NPar);
   gMinuit->SetFCN(fcn);
+#endif
 
   double arglist[10];
 #ifdef __EFF__
@@ -244,10 +281,12 @@ void Ckstar()
   double p1 = 0;
   int npars = NPar;
   int matrix = 0;
+#ifdef __FIT__
   for(int i=0;i<NPar;i++) {
     gMinuit->mnparm( i, PARM_NAMES[i], var[i], verr[i], var_min[i], var_max[i], ivarbl);
+    gMinuit->FixParameter(4);
+    gMinuit->FixParameter(6);
   }
-  
 
   arglist[0] = 0.0001;
   gMinuit->mnexcm("SET EPSmachine", arglist, 2, ierflg );
@@ -266,21 +305,26 @@ void Ckstar()
 
   for(int i=0;i<NPar;i++) {
     gMinuit->GetParameter(i, var[i], verr[i]);
+    std::cout << PARM_NAMES[i] << "\t" << var[i] << " +/- " << verr[i] << std::endl;
   }
 
+#ifdef __CONTOUR__
   TGraph *gr_err[3];
   for(int i=0;i<3;i++) {
     //    if(i!=0) continue;
     gMinuit->SetErrorDef((i+1)*(i+1));
     gr_err[i] = (TGraph *)gMinuit->Contour(100, 3, 5);
   }
+#endif
+  
+#endif  
   
   for(int ip=0;ip<NC;ip++) {
     c1->cd(ip+1);
   
-    TH1D *hist = new TH1D("hist","",1,0,100);
+    TH1D *hist = new TH1D("hist","",1,0,KMAX);
     hist->SetMinimum(0.);
-    hist->SetMaximum(15.);
+    hist->SetMaximum(5.);
     hist->GetXaxis()->SetLabelOffset(0.015);
     hist->GetXaxis()->SetTitleOffset(1.0);
     hist->GetXaxis()->SetTitle("k* (MeV/c)");
@@ -310,27 +354,30 @@ void Ckstar()
     cf_fun_0[ip]->SetLineStyle(2);
     cf_fun_0[ip]->SetLineWidth(3);
     cf_fun_0[ip]->Draw("c same");
-    //    gr_data[ip]->SetMarkerSize(1.5);
-    //    gr_data[ip]->SetMarkerStyle(20);
+    gr_data[ip]->SetMarkerSize(1.0);
+    gr_data[ip]->SetMarkerStyle(20);
     gr_data[ip]->Draw("p");
 
     drawText(70, 12, LabelName[ip], 42, 0.08);
     drawText(60, 9, Form("R_{G} = %4.2f #pm %4.2f fm", var[ip], verr[ip]), 42, 0.065);
   }
+  double chi2 = calChi2(var);
+  cout<<"chi2 / NDF = "<<chi2 << " / " << (gr_data[0]->GetN()*3-NPar+2)<<endl;
 
   c1->Update();
   c1->SaveAs("CF_dl.pdf");
   c1->SaveAs("CF_dl.png");
 
 
+#ifdef __CONTOUR__
   TCanvas *c2 = new TCanvas("c2","c2",600,0,600,600);
   c1->SetBottomMargin(0.1);
   c1->SetLeftMargin(0.1);
   c1->Draw();
 
-  TH1D *hC = new TH1D("hC","", 1, -40, 0);
+  TH1D *hC = new TH1D("hC","", 1, -100, 0);
   hC->SetMinimum(0);
-  hC->SetMaximum(40);
+  hC->SetMaximum(20);
   hC->GetXaxis()->SetLabelOffset(0.01);
   hC->GetXaxis()->SetTitle("f_{D} (fm)");
   hC->GetYaxis()->SetTitle("f_{Q} (fm)");
@@ -343,6 +390,7 @@ void Ckstar()
     gr_err[i]->Draw("c");
   }
 
-  drawHistBox(-40, 0, 0, 40);
-  
+  drawHistBox(-100, 0, 0, 20);
+#endif
+
 }
