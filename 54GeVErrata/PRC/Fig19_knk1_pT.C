75112a152f709f363efa89a04b26a5b14b4a404c
starsdong <XDong@lbl.gov>
2024-09-04 09:39:41 -0700

    added macros for Fig.19 and 20, slight adjustment for Fig.16

--- 54GeVErrata/PRC/Fig19_knk1_pT.C
+++ 54GeVErrata/PRC/Fig19_knk1_pT.C
@@ -1,7 +1,7 @@
 #include "draw.C+"
 #include "style.C+"
 
-void Fig16_knk1_y()
+void Fig19_knk1_pT()
 {
   style();
 
@@ -17,30 +17,31 @@ void Fig16_knk1_y()
   const Int_t NP = 2; // number of particle categories: proton, anti-proton
   const Char_t *PName[NP] = {"Pro", "Apro"};
   const Char_t *PName_54[NP] = {"pro", "antipro"};
-  const Int_t NY = 5;  // rapidity bins
-  const Double_t RAP[NY] = {0.1, 0.2, 0.3, 0.4, 0.5};
+  const Int_t NPt = 5;  // pT bins
+  const Double_t PT[NPt] = {1.0, 1.2, 1.4, 1.6, 2.0};
+  const Double_t PTDIS[NPt] = {0.5, 1.0, 1.5, 2.0, 3.0}; // for display purpose
   const Int_t NCen = 9; // 9 centrality bins
-  const double rap_offset[NP] = {0.008, 0.000}; // different rapidity offsets for different particles for plotting
-  double rap[NP][NY];
+  const double pT_offset[NP] = {0., 0.015}; // different pT offsets for different particles for plotting
+  double pT[NP][NPt];
   for(int i=0;i<NP;i++) {
-    for(int j=0;j<NY;j++) {
-      rap[i][j] = RAP[j] + rap_offset[i];
+    for(int j=0;j<NPt;j++) {
+      pT[i][j] = PTDIS[j] + pT_offset[i];
     }
   }
-  
+
   
   ////////////////////////////////////////////////
   // Read in data: 54 GeV stored differently
   ////////////////////////////////////////////////
   TGraphErrors *gr_stat[NE][NCum][NP], *gr_sys[NE][NCum][NP];  // all central collisions
-  double cum[NE][NCum+1][NP][NY], cum_e[NE][NCum+1][NP][NY], cum_e_sys[NE][NCum+1][NP][NY];   // name is cum, actually are kappa in this Fig.
-  double cumR[NE][NCum][NP][NY], cumR_e[NE][NCum][NP][NY], cumR_e_sys[NE][NCum][NP][NY]; // ratios to k1
-  TFile *fin[NE][NP][NY];
+  double cum[NE][NCum+1][NP][NPt], cum_e[NE][NCum+1][NP][NPt], cum_e_sys[NE][NCum+1][NP][NPt];   // name is cum, actually are kappa in this Fig.
+  double cumR[NE][NCum][NP][NPt], cumR_e[NE][NCum][NP][NPt], cumR_e_sys[NE][NCum][NP][NPt]; // ratios to k1
+  TFile *fin[NE][NP][NPt];
   for(int i=0;i<NE;i++) {
     if(i!=index_54) {
       for(int j=0;j<NP;j++) {
-	for(int k=0;k<NY;k++) {
-	  fin[i][j][k] = new TFile(Form("rootfile_0517/final_corrfun_ycut/%sGeV/AuAu_sys_%s_Y%3.1f.root",EneDir[i],PName[j],RAP[k]));
+	for(int k=0;k<NPt;k++) {
+	  fin[i][j][k] = new TFile(Form("rootfile_0517/final_corrfun_ptcut/%sGeV/AuAu_sys_%s_pt%3.1f.root",EneDir[i],PName[j],PT[k]));
 	  TGraphErrors *gr_stat_a[NCum], *gr_sys_a[NCum];
 	  for(int m=0;m<NCum;m++) {
 	    gr_stat_a[m] = (TGraphErrors *)fin[i][j][k]->Get(Form("K%d1_stat",m+2));
@@ -49,11 +50,6 @@ void Fig16_knk1_y()
 	    cumR_e[i][m][j][k] = gr_stat_a[m]->GetEY()[0];
 	    cumR_e_sys[i][m][j][k] = gr_sys_a[m]->GetEY()[0];
 	    //	    cout << EneDir[i] << " " << PName[j] << " " << rap[k] << Form(" C%d",m+1) << " " << cum[i][m][j][k] << endl;
-	    
-	    // cumR[i][m][j][k] = cum[i][m][j][k]/cum[i][0][j][k];
-	    // cumR_e[i][m][j][k] = cumR[i][m][j][k] * sqrt(pow(cum_e[i][m][j][k]/cum[i][m][j][k], 2.0)+pow(cum_e[i][0][j][k]/cum[i][0][j][k], 2.0));
-	    // cumR_e_sys[i][m][j][k] = cumR[i][m][j][k] * sqrt(pow(cum_e_sys[i][m][j][k]/cum[i][m][j][k], 2.0)+pow(cum_e_sys[i][0][j][k]/cum[i][0][j][k], 2.0));
-	    
 	    if(i==0&&m==NCum-1) { // 7.7 GeV C4 data points scaled down by 0.5
 	      cumR[i][m][j][k] *= 0.5;
 	      cumR_e[i][m][j][k] *= 0.5;
@@ -61,13 +57,13 @@ void Fig16_knk1_y()
 	    }
 	  } // end m->NCum
 	  fin[i][j][k]->Close();
-	} // end k->NY
-	
+	} // end k->NPt
+
 	for(int m=0;m<NCum;m++) {
-	  gr_stat[i][m][j] = new TGraphErrors(NY, rap[j], cumR[i][m][j], 0, cumR_e[i][m][j]);
-	  gr_sys[i][m][j] = new TGraphErrors(NY, rap[j], cumR[i][m][j], 0, cumR_e_sys[i][m][j]);
-	  cout << "++ " << EneDir[i] << " " << PName[j] << Form(" k%d/k1",m+2) << endl;
-	  gr_stat[i][m][j]->Print();
+	  gr_stat[i][m][j] = new TGraphErrors(NPt, pT[j], cumR[i][m][j], 0, cumR_e[i][m][j]);
+	  gr_sys[i][m][j] = new TGraphErrors(NPt, pT[j], cumR[i][m][j], 0, cumR_e_sys[i][m][j]);
+	  //	  cout << "++ " << EneDir[i] << " " << PName[j] << Form(" C%d",m+1) << endl;
+	  //	  gr_stat[i][m][j]->Print();
 	}
       } // end j->NP
     } else { // 54 GeV data
@@ -75,30 +71,31 @@ void Fig16_knk1_y()
       for(int j=0;j<NP;j++) {
 	TGraphErrors *gr_stat_b[NCum+1], *gr_sys_b[NCum+1];
 	for(int m=0;m<NCum+1;m++) {
-	  gr_stat_b[m] = (TGraphErrors *)fin_54->Get(Form("rapidity_correl_%s_K%d_stat",PName_54[j], m+1));
-	  gr_sys_b[m] = (TGraphErrors *)fin_54->Get(Form("rapidity_correl_%s_K%d_sys",PName_54[j], m+1));
-	  
-	  for(int k=0;k<NY;k++) {
+	  gr_stat_b[m] = (TGraphErrors *)fin_54->Get(Form("pT_correl_%s_K%d_stat",PName_54[j], m+1));
+	  gr_sys_b[m] = (TGraphErrors *)fin_54->Get(Form("pT_correl_%s_K%d_sys",PName_54[j], m+1));
+
+	  for(int k=0;k<NPt;k++) {
 	    cum[i][m][j][k] = gr_stat_b[m]->GetY()[k];
 	    cum_e[i][m][j][k] = gr_stat_b[m]->GetEY()[k];
 	    cum_e_sys[i][m][j][k] = gr_sys_b[m]->GetEY()[k];
 	  }
 	}
+
 	for(int m=0;m<NCum;m++) {
-	  for(int k=0;k<NY;k++) {
+	  for(int k=0;k<NPt;k++) {
 	    cumR[i][m][j][k] = cum[i][m+1][j][k]/cum[i][0][j][k];
 	    cumR_e[i][m][j][k] = fabs(cumR[i][m][j][k]) * sqrt(pow(cum_e[i][m+1][j][k]/cum[i][m+1][j][k], 2.0)+pow(cum_e[i][0][j][k]/cum[i][0][j][k], 2.0));
 	    cumR_e_sys[i][m][j][k] = fabs(cumR[i][m][j][k]) * sqrt(pow(cum_e_sys[i][m+1][j][k]/cum[i][m+1][j][k], 2.0)+pow(cum_e_sys[i][0][j][k]/cum[i][0][j][k], 2.0));
 	  }
 	  
-	  gr_stat[i][m][j] = new TGraphErrors(NY, rap[j], cumR[i][m][j], 0, cumR_e[i][m][j]);
-	  gr_sys[i][m][j] = new TGraphErrors(NY, rap[j], cumR[i][m][j], 0, cumR_e_sys[i][m][j]);
+	  gr_stat[i][m][j] = new TGraphErrors(NPt, pT[j], cumR[i][m][j], 0, cumR_e[i][m][j]);
+	  gr_sys[i][m][j] = new TGraphErrors(NPt, pT[j], cumR[i][m][j], 0, cumR_e_sys[i][m][j]);	
 	} // end m->NCum
 	
       } // end j->NP    
     } // end if(i!=index_54)
   } // end i->NE
-  
+
   ////////////////////////////////////////////////
   // Start Plotting
   ////////////////////////////////////////////////
@@ -107,18 +104,18 @@ void Fig16_knk1_y()
   TH1D *h0[NE][NCum];
   const double pad_x1 = 0.06;
   const double pad_y1 = 0.10; // x,y pad offsets
-  const double pad_x_gap = 0.; // gap between 27 GeV and 39 GeV (index 4-5)
   const double pad_x2 = 0.99;
   const double pad_y2 = 0.98;
-  const int pad_gap_index = 5; // starting from index 5 (39 GeV)
-  const double ymin[NCum] = {-0.11, -0.16, -1.1};
-  const double ymax[NCum] = {0.031, 0.16, 1.5};
-  const double xmin = 0.05;
-  const double xmax = 0.55;
+  const double ymin[NCum] = {-0.11, -0.16, -0.9};
+  const double ymax[NCum] = {0.031, 0.183, 1.9};
+  const double xmin = 0.25;
+  const double xmax = 3.25;
   const Int_t kColor[NP] = {kBlue, kBlack};
   const Int_t kStyle[NP] = {21, 34};
   const Double_t kSize[NP] = {0.8, 1.2};
+  const Int_t NDiv[NCum] = {504, 504, 504};
 
+  
   ////////////////////////////////////////////////
   // Draw main panels
   ////////////////////////////////////////////////
@@ -126,12 +123,8 @@ void Fig16_knk1_y()
     for(int j=0;j<NCum;j++) {
       c1->cd();
       
-      double x1 = pad_x1 + i*(pad_x2 - pad_x1 - pad_x_gap)/NE;
-      double x2 = pad_x1 + (i+1)*(pad_x2 - pad_x1 - pad_x_gap)/NE;
-      if(j>=pad_gap_index) {
-	x1 += pad_x_gap;
-	x2 += pad_x_gap;
-      }
+      double x1 = pad_x1 + i*(pad_x2 - pad_x1)/NE;
+      double x2 = pad_x1 + (i+1)*(pad_x2 - pad_x1)/NE;
       double y1 = pad_y2 - (j+1)*(pad_y2 - pad_y1)/NCum;
       double y2 = pad_y2 - j*(pad_y2 - pad_y1)/NCum;
       pad[i][j] = new TPad(Form("pad_%d_%d",i,j),"",x1,y1,x2,y2);
@@ -145,10 +138,10 @@ void Fig16_knk1_y()
       h0[i][j] = new TH1D(Form("h0_%d_%d",i,j),"",1,xmin,xmax);
       h0[i][j]->SetMaximum(ymax[j]);
       h0[i][j]->SetMinimum(ymin[j]);
-      h0[i][j]->GetXaxis()->SetNdivisions(404);
+      h0[i][j]->GetXaxis()->SetNdivisions(408);
       h0[i][j]->GetXaxis()->SetTitleOffset(999.);
       h0[i][j]->GetXaxis()->SetLabelOffset(999.);
-      h0[i][j]->GetYaxis()->SetNdivisions(505);
+      h0[i][j]->GetYaxis()->SetNdivisions(NDiv[j]);
       h0[i][j]->GetYaxis()->SetTitleOffset(999.);
       h0[i][j]->GetYaxis()->SetLabelOffset(999.);
       h0[i][j]->Draw("c");
@@ -158,7 +151,7 @@ void Fig16_knk1_y()
 	cout << "++ " << EneDir[i] << " " << PName[k] << Form(" k%d/k1",j+2) << endl;
 	gr_stat[i][j][k]->Print();
 
-	drawSysError(gr_sys[i][j][k], 0.01, (ymax[j]-ymin[j])*0.02, kColor[k]);
+	drawSysError(gr_sys[i][j][k], 0.06, (ymax[j]-ymin[j])*0.02, kColor[k]);
 
 	setGraphMarker(gr_stat[i][j][k], kStyle[k], kColor[k], kSize[k]);
 	setGraphLine(gr_stat[i][j][k], 1, kColor[k], 1);
@@ -166,21 +159,13 @@ void Fig16_knk1_y()
 
       }
 
-      /*
-      TGraphErrors *gr_clone = (TGraphErrors *)gr_stat[i][j][NP-1]->Clone(Form("%s_clone",gr_stat[i][j][NP-1]->GetName()));
-      // net-p replotting
-      setGraphMarker(gr_clone, 24, kBlack, kSize[NP-1]+0.15);
-      setGraphLine(gr_clone, 1, kRed, 1);
-      gr_clone->Draw("p");
-      */
-
       if(j==0) {
-	drawText(0.18, (ymax[j]-ymin[j])*0.88+ymin[j], Form("%s GeV", EneLabel[i]), 42, 0.14);
+	drawText(xmin+(xmax-xmin)*0.3, (ymax[j]-ymin[j])*0.88+ymin[j], Form("%s GeV", EneLabel[i]), 42, 0.14);
       }
 
       if(i==0&&j==NCum-1) { // 7.7 GeV C4 legend
-	drawText(0.1, 1.15, "#kappa_{4}/#kappa_{1} #times 0.5", 42, 0.14);
-	drawText(0.1, 0.8, "7.7 GeV only", 42, 0.14);
+	drawText(xmin+(xmax-xmin)*0.08, 1.5, "#kappa_{4}/#kappa_{1} #times 0.5", 42, 0.13);
+	drawText(xmin+(xmax-xmin)*0.08, 1.1, "7.7 GeV only", 42, 0.13);
       }
       
       pad[i][j]->Update();
@@ -191,15 +176,16 @@ void Fig16_knk1_y()
   // Draw axis labels and titles
   ////////////////////////////////////////////////
   c1->cd();
-  TPad *pad_x = new TPad("pad_x", "", pad_x1, 0, pad_x2, pad_y1-0.003);
+  TPad *pad_x = new TPad("pad_x", "", pad_x1, 0, 1.0, pad_y1-0.003);
   pad_x->Draw();
   pad_x->cd();
 
   for(int i=0;i<NE;i++) {
-    drawText(i*1./NE + 0.024, 0.7, "0.2", 42, 0.35);
-    drawText(i*1./NE + 0.069, 0.7, "0.4", 42, 0.35);
+    drawText(i*1./NE*(pad_x2-pad_x1)/(1-pad_x1) + 0.018, 0.75, "1.2", 42, 0.33);
+    drawText(i*1./NE*(pad_x2-pad_x1)/(1-pad_x1) + 0.054, 0.75, "1.6", 42, 0.33);
+    drawText(i*1./NE*(pad_x2-pad_x1)/(1-pad_x1) + 0.091, 0.75, "2.0", 42, 0.33);
   }
-  drawText(0.43, 0.25, "Rapidity Cut y_{max}", 42, 0.43);
+  drawText(0.33, 0.25, "Transverse Momentum Cut p_{T}^{max} (GeV/c)", 42, 0.43);
   pad_x->Update();
   
   c1->cd();
@@ -208,14 +194,14 @@ void Fig16_knk1_y()
   pad_y->cd();
 
   const Int_t NYLabelMax = 5;
-  const Int_t NYLabel[NCum] = {3, 3, 3};
+  const Int_t NYLabel[NCum] = {3, 3, 2};
   const Char_t *YLabel[NCum][NYLabelMax] = {{"#font[122]{-} 0.10","#font[122]{-} 0.05","   0.00","",""},
 					    {"#font[122]{-} 0.1","   0.0","   0.1","",""},
-					    {"#font[122]{-} 1","   0","   1","",""}
+					    {"   0","   1","","",""}
   };
   const Double_t XOffset[NCum] = {0.01, 0.12, 0.27};
-  const Double_t Offset[NCum] = {0.018, 0.055, 0.005};
-  const Double_t Step[NCum] = {0.115, 0.102, 0.125}; 
+  const Double_t Offset[NCum] = {0.018, 0.052, 0.098};
+  const Double_t Step[NCum] = {0.115, 0.096, 0.118}; 
   for(int i=0;i<NCum;i++) {
     for(int j=0;j<NYLabel[i];j++) {
       drawText(0.45+XOffset[i], (NCum-1-i)*1./NCum + j*Step[i] + Offset[i], YLabel[i][j], 42, 0.22);
@@ -228,12 +214,12 @@ void Fig16_knk1_y()
   // Draw other legends and labels
   ////////////////////////////////////////////////
   c1->cd();
-  TPad *pad_coll = new TPad("pad_coll","", 0.365, 0.58, 0.72, 0.67);
+  TPad *pad_coll = new TPad("pad_coll","", 0.38, 0.58, 0.67, 0.67);
   pad_coll->Draw();
   pad_coll->cd();
 
-  drawText(0.01, 0.6, "STAR Au+Au Collisions 0-5% most central", 42, 0.48);
-  drawText(0.12, 0.18, "0.4 < p_{T} < 2.0 (GeV/c), |y| < y_{max}", 42, 0.45);
+  drawText(0.01, 0.6, "STAR Au+Au Collisions 0-5% most central", 42, 0.45);
+  drawText(0.12, 0.18, "0.4 < p_{T} < p_{T}^{max} (GeV/c), |y| < 0.5", 42, 0.43);
   pad_coll->Update();
 
   c1->cd();
@@ -244,8 +230,8 @@ void Fig16_knk1_y()
   TLegend *leg[NP];
   const Int_t index_leg[NP] = {0, 1};
   const Char_t *name_leg[NP] = {" Proton", " Anti-proton"};
-  const Double_t x1_leg[NP] = {0.1, 0.55};
-  const Double_t x2_leg[NP] = {0.5, 0.9};
+  const Double_t x1_leg[NP] = {0.1, 0.5};
+  const Double_t x2_leg[NP] = {0.45, 0.9};
   TGraphErrors *gr_leg[NP];
   for(int i=0;i<NP;i++) {
     leg[i] = new TLegend(x1_leg[i], 0.1, x2_leg[i], 0.9);
@@ -256,13 +242,12 @@ void Fig16_knk1_y()
     leg[i]->AddEntry(gr_leg[i], name_leg[i], "p");
     leg[i]->Draw();
   }
-  
   pad_leg->Update();
   c1->cd();
 
   c1->Update();
-  c1->SaveAs("fig/Fig16_knk1_y.pdf");
-  c1->SaveAs("fig/Fig16_knk1_y.png");
-  
+  c1->SaveAs("fig/Fig19_knk1_pT.pdf");
+  c1->SaveAs("fig/Fig19_knk1_pT.png");
+
   
 }
